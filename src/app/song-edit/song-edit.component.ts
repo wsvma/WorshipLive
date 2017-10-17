ee7dce0c4e05570f5475053c54d4f88749a3afc9
Vincent <vincentma@alumni.cmu.edu>
2017-10-17 09:57:25 -0700

    Added more fields to edit in song edit page

--- src/app/song-edit/song-edit.component.ts
+++ src/app/song-edit/song-edit.component.ts
@@ -1,10 +1,16 @@
 import { Subscription } from 'rxjs/Rx';
 import { Song, SongInDb } from '../../models/song';
 import { SongsService } from '../songs.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, PACKAGE_ROOT_URL } from '@angular/core';
+
+interface EditField {
+  name: string;
+  label: string;
+  style: { width: string };
+}
 
 @Component({
   selector: 'app-song-edit',
@@ -14,24 +20,62 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 export class SongEditComponent implements OnInit, OnDestroy {
 
   songId : string;
-  song : Song = new Song(new SongInDb());
+  song : Song;
+  original: Song;
   subscription: Subscription;
 
-  constructor(private route: ActivatedRoute, private songService: SongsService) {
+  editFields = [
+    { name: 'title_1'       , label: 'Title 1'      , style: { width: '25%' } },
+    { name: 'title_2'       , label: 'Title 2'      , style: { width: '25%' } },
+    { name: 'writer'        , label: 'Writer'       , style: { width: '25%' } },
+    { name: 'category'      , label: 'Category'     , style: { width: '25%' } },
+    { name: 'key'           , label: 'Key'          , style: { width: '12%' } },
+    { name: 'capo'          , label: 'Capo'         , style: { width: '12%' } },
+    { name: 'timing'        , label: 'Timing'       , style: { width: '16%' } },
+    { name: 'copyright'     , label: 'Copyright'    , style: { width: '30%' } },
+    { name: 'license_admin1', label: 'License Admin', style: { width: '30%' } },
+  ];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private songService: SongsService) {
     this.songId = this.route.snapshot.paramMap.get('id');
-    console.log("constructor:", this.songId);
+    this.song = this.original = new Song(new SongInDb());
   }
 
   ngOnInit() {
     this.subscription = this.songService.get(this.songId).subscribe((song: Song) => {
-      console.log("observer:", song);
       this.song = song;
+      this.original = {...song};
     });
-    console.log("oninit");
   }
 
   ngOnDestroy() {
-    console.log("destroy");
     this.subscription.unsubscribe();
+  }
+
+  isModified() {
+    let fieldsToCompare = this.editFields.map(f => f.name);
+    fieldsToCompare.push('lyrics');
+    for (let field of fieldsToCompare) {
+      if (!(this.original[field] === this.song[field]))
+        return true;
+    }
+    return false;
+  }
+
+  onScroll($event) {
+    let otherId = ($event.target.id == 'lyrics1') ? 'lyrics2' : 'lyrics1';
+    let otherTextArea = document.getElementById(otherId);
+    otherTextArea.scrollTop = $event.target.scrollTop;
+  }
+
+  navigateBack() {
+    this.router.navigateByUrl('/songs');
+  }
+
+  saveChanges() {
+    this.songService.update(this.song);
   }
 }

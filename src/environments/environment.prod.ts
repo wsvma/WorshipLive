cd2f36b37b2234a8f39d3761d16239a7fb9fe102
Vincent Ma <vincentma@alumni.cmu.edu>
2019-12-19 15:56:06 -0800

    Move firebase config to separate file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
+import { firebaseConfig } from './firebase.config'
+
 export const environment = {
   production: true,
-  firebaseConfig : {
-    apiKey: "AIzaSyAcqrVoHm-SfgickuqCjS_cKCsymKwHY5w",
-    authDomain: "trydb-75773.firebaseapp.com",
-    databaseURL: "https://trydb-75773.firebaseio.com",
-    projectId: "trydb-75773",
-    storageBucket: "trydb-75773.appspot.com",
-    messagingSenderId: "7391445985",
-    appId: "1:7391445985:web:581aa8b9585a62384508a0"
-  }
+  firebaseConfig : firebaseConfig,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,9 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+import { firebaseConfig } from './firebase.config'
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyAcqrVoHm-SfgickuqCjS_cKCsymKwHY5w",
-    authDomain: "trydb-75773.firebaseapp.com",
-    databaseURL: "https://trydb-75773.firebaseio.com",
-    projectId: "trydb-75773",
-    storageBucket: "trydb-75773.appspot.com",
-    messagingSenderId: "7391445985",
-    appId: "1:7391445985:web:581aa8b9585a62384508a0"
-  }
+  firebaseConfig : firebaseConfig,
 };
